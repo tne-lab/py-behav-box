0bf22097b182a6ec7f401e68fdb200c8ef9efcf2
Mark Schatza <markschatza@gmail.com>
2018-11-08 15:14:04 -0600

    Log file -> csv data file built (very basic)

--- TNEL-pyBox/csv/test.py
+++ TNEL-pyBox/csv/test.py
@@ -1,10 +1,55 @@
 import pandas
-import random
+import ast
 
-random.seed()
+def parseLog():
+    trials = []
+    i = -1
+    with open('log.txt','r') as f:
+        for line in f:
+            line = line.strip()
+            split = line.split(',')
+            if 'trial' in split[0]:
+                i+=1
+                trials.append({split[0] : float(split[1])})
+            else:
+                if split[0]=='conditionInfo':
+                    trials[i][split[0]] = ast.literal_eval(split[1])
+                else:
+                    trials[i][split[0]] = float(split[1])
+    return trials
 
-df = pandas.read_csv('data.csv',index_col='trial')
-for i in range(1,len(df['shock'])-1):
-    df['shock'][i] = random.randint(0,1)
-    
-print(df)
+def parseTrials(trials):
+    data = {'trial number': [], 'response' : [],'correct response' : [],'outcome' : [],'time from start' : []}
+    i = 0
+    for trial in trials:
+        startTime = 0
+
+        for key in trial.keys():
+            if 'trial' in key:
+                data['trial number'].append(key[-1])
+                startTime = trial[key]
+            if key == trial['conditionInfo']['desired response']:
+                data['correct response'].append('True')
+            if key == 'lever-press-right' or key == 'lever-press-left':
+                data['response'].append(key)
+                data['time from start'].append(trial[key] - startTime)
+                if len(data['correct response']) == i+1:
+                    continue
+                else:
+                    data['correct response'].append('False')
+
+            if key == 'food' or key == 'shock':
+                data['outcome'].append(key)
+        i+=1
+    return data
+
+
+def main():
+    trials = parseLog()
+    data = parseTrials(trials)
+
+    df = pandas.DataFrame(data,columns = ['response','correct response','outcome','time from start'], index = data['trial number'])
+    df.index.name = 'Trial Number'
+    df.to_csv('ex.csv')
+
+main()
