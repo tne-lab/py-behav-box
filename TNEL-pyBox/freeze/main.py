ad4192035e56e84cdb7b6e0cba23864c67ef3309
marks <markschatza@gmail.com>
2018-11-21 12:30:54 -0600

    loggin freezes

--- TNEL-pyBox/freeze/main.py
+++ TNEL-pyBox/freeze/main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-from vidClass import Vid
 import argparse
 import datetime
 import imutils
@@ -9,14 +8,22 @@ import time
 class Vid:
     def __init__(self, videoPath, winName):
         self.cap = cv2.VideoCapture(videoPath)
-        self.startFrame = 0
-        self.fps = int((1/self.cap.get(cv2.cv2.CAP_PROP_FPS))*1000)
-        self.cap.set(1,self.startFrame)
-        self.winName = winName
-        self.length = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        cv2.namedWindow(self.winName)
-        self.min = 0
-        self.firstFrame = None
+        if self.cap.isOpened():
+            self.startFrame = 0
+            self.spf = int((1/self.cap.get(5))*1000)
+            self.cap.set(1,self.startFrame)
+            self.winName = winName
+            self.length = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            cv2.namedWindow(self.winName)
+            self.min = 0
+            self.timeFrozen = 0
+            self.text = ''
+            self.capError = False
+            self.freezeFile = open('freezes.txt','w')
+            self.isFrozen = False
+            self.threshold = 25
+        else:
+            self.capError = True
 
     def trackBar(self,winName,trackName,min, max):
         cv2.createTrackbar(trackName, winName,min,max,
@@ -27,56 +34,91 @@ class Vid:
         self.cap.set(1,self.startFrame)
 
     def run(self):
+        noROI = True
+        noPrevFrame = True
+        noPrevThresh = True
         while(self.cap.isOpened()):
             #Get frame
             ret, frame = self.cap.read()
-            try:
-                imgROI = frame[int(r[1]):int(r[1]+r[3]),int(r[0]):int(r[0] + r[2])]
-            except:
-                r = cv2.selectROI(frame)
-                cv2.destroyWindow("ROI selector")
-                continue
-
             if not ret:
                 break
-            text = 'freeze'
+
+            # Create ROI if not done yet
+            # Otherwise grab ROI
+            if noROI:
+                r = cv2.selectROI(frame)
+                cv2.destroyWindow("ROI selector")
+                noROI = False
+                continue
+            else:
+                imgROI = frame[int(r[1]):int(r[1]+r[3]),int(r[0]):int(r[0] + r[2])]
+
             #Make gray and blur
             gray = cv2.cvtColor(imgROI, cv2.COLOR_BGR2GRAY)
             gray = cv2.GaussianBlur(gray, (21, 21), 0)
             cv2.imshow(self.winName,gray)
 
 
-            self.startFrame+=1
-            if self.firstFrame is None:
-            		self.firstFrame = gray
-            		continue
+            if noPrevFrame:
+                prevFrame = gray
+                noPrevFrame = False
+                continue
             else:
-                #Start motion detection
-            	# compute the absolute difference between the current frame and
-            	# first frame
-                frameDelta = cv2.absdiff(self.firstFrame, gray)
-                self.firstFrame = gray
-                thresh = cv2.threshold(frameDelta, 23, 255, cv2.THRESH_BINARY)[1]
-
-                try:
-                    diff = cv2.subtract(thresh, prevThres)
-                    moving_pixels = cv2.countNonZero(diff)
-                    print(moving_pixels)
-                    prevThres = thresh
-                except:
-                    prevThres = thresh
+                if noPrevThresh:
+                    prevThresh = self.calcInitThresh(gray, prevFrame)
+                    noPrevThresh = False
                     continue
+                else:
+                    movingPxls, prevThresh, prevFrame = self.calcMovingPixels(gray, prevFrame, prevThresh)
 
+            # Moving or frozen?
+            if movingPxls>40:
+                self.text = 'move'
+                self.timeFrozen = 0
+                if self.isFrozen:
+                    self.isFrozen = False
+                    self.freezeFile.write('end freeze: ' + str(self.cap.get(0)) + '\n')
+            else:
 
-            if moving_pixels>40:
-                text = 'move'
+                if self.checkFreeze() and not self.isFrozen:
+                    self.isFrozen = True
+                    self.freezeFile.write('freeze: ' + str(self.cap.get(0)) + '\n')
+                    print('freeze')
+                    self.text = 'freeze'
 
-            cv2.putText(thresh,text, (10, 50),cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 2)
-            cv2.imshow('thresh',thresh)
-            if cv2.waitKey(self.fps) & 0xFF == ord('q'):
+            # Write freeze/move on screen
+            cv2.putText(prevThresh, self.text, (10, 50),cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 2)
+            cv2.imshow('thresh',prevThresh)
+            # Get next frame
+            self.startFrame+=1
+            if cv2.waitKey(self.spf) & 0xFF == ord('q'):
                 break
 
+    def calcInitThresh(self, frame, prevFrame):
+        frameDelta = cv2.absdiff(frame, prevFrame)
+        return cv2.threshold(frameDelta, self.threshold, 255, cv2.THRESH_BINARY)[1]
+
+    def calcMovingPixels(self, frame, prevFrame, prevThresh):
+        #Start motion detection
+        # compute the absolute difference between the current frame and
+        # previous frame
+        frameDelta = cv2.absdiff(frame, prevFrame)
+        thresh = cv2.threshold(frameDelta, self.threshold, 255, cv2.THRESH_BINARY)[1]
+
+        diff = cv2.subtract(thresh, prevThresh)
+        movingPxls = cv2.countNonZero(diff)
+        return movingPxls, thresh, frame
+
+    def checkFreeze(self):
+        if self.timeFrozen < 2000:
+            self.timeFrozen+=self.spf
+            print('freeze time: ' + str(self.timeFrozen))
+            return False
+        else:
+            return True
+
     def close(self):
+        self.freezeFile.close()
         self.cap.release()
         cv2.destroyAllWindows()
 
@@ -84,9 +126,10 @@ class Vid:
 
 def main():
     vid = Vid('FR_con1.avi','vid')
-    vid.trackBar('vid','frameNumber',0, vid.length)
-    vid.run()
-    vid.close()
+    if not vid.capError:
+        vid.trackBar('vid','frameNumber',0, vid.length)
+        vid.run()
+        vid.close()
 
 
 main()
