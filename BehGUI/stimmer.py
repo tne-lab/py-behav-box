bdd45e02cef07de2937057741322b48961739332
fjkdasilva <fjkdasilva@gmail.com>
2019-04-25 15:31:07 -0500

    Stimmer tested on flavs, need to validate pulse

--- BehGUI/stimmer.py
+++ BehGUI/stimmer.py
@@ -5,15 +5,17 @@ import os
 import zmq
 import math
 
+# Dev3 for flavs computer
+
 class Stim:
     '''
     Class to create and send stim waveforms
     '''
-    def __init__(self.address, q):
+    def __init__(self, address, q):
         ######## Create Waveform ######################################
         # --------------------- INPUTS --------------------------------
-        sr = 100000 # Sampling Rate (Hz)
-        amplitude = 1 # Amplitude (Volts)
+        sr = 10000 # Sampling Rate (Hz)
+        amplitude = 10 # Amplitude (Volts)
         width = 10 # duration of pulse (ms)
         ipi = 30 # inter-pulse interval (ms). output zero during this period,
         # if muliple waveforms, period = width + ipi
@@ -23,7 +25,7 @@ class Stim:
         # --------------------------------------------------------------
 
         # Now lets build it
-        waveform = []
+        self.waveform = []
         negAmplitude = amplitude * -1
         widthSamp = int(width/1000.0*sr)
         ipiSamp = int(ipi/1000.0*sr)
@@ -31,48 +33,49 @@ class Stim:
 
         for num in range(numPulse):
             for i in range(int(phaseShift/360.0 * period)):
-                waveform.append(0)
+                self.waveform.append(0)
             for i in range(widthSamp):
                 if biphasic:
                     if i <= math.ceil(widthSamp/2) + 1:
-                        waveform.append(amplitude)
+                        self.waveform.append(amplitude)
                     else:
-                        waveform.append(negAmplitude)
+                        self.waveform.append(negAmplitude)
                 else:
-                    waveform.append(amplitude)
+                    self.waveform.append(amplitude)
             for i in range(int((360-phaseShift)/360.0 * period)):
-                waveform.append(0)
+                self.waveform.append(0)
         ##################################################################
 
         # From parent to get quit
         self.q = q
 
         # Create socket to listen to
+        '''
         context = zmq.Context()
         self.socket = context.socket(zmq.SUB, ZMQ_RCVTIMEO = 30)
         self.socket.connect("tcp://localhost:" + str(port))
         SUBSCRIBE = b'event'
         for sub in SUBSCRIBE:
             self.socket.setsockopt(zmq.SUBSCRIBE, sub)
-
+            '''
 
         # Create Task
         self.task = nidaqmx.Task()
-        self.task.ao_channels.add_ao_voltage_chan(address, min_val = 0,
-            max_val = amplitude) # Change max_val to max voltage used
+        self.task.ao_channels.add_ao_voltage_chan(address) # Change max_val to max voltage used
         # Set timing
-        self.task.timing(sr) # rate , can also change active_edge,
+        self.task.timing.cfg_samp_clk_timing(sr, samps_per_chan = period) # rate , can also change active_edge,
                                 #continuous or finite number of samples
 
         #self.waitForEvent()
 
 
-    def sendStim():
-        self.task.write(self.waveform, auto_start=True)
+    def sendStim(self):
+        self.task.write(self.waveform, auto_start = True)
         self.task.wait_until_done() # Waits until done or timeouts after 10 seconds
         self.task.stop()
+        print('stim done')
 
-    def waitForEvent():
+    def waitForEvent(self):
         while True:
             msg = self.socket.recv()
             if len(msg) == 2:
@@ -85,9 +88,15 @@ class Stim:
             if not self.q.empty():
                 return
 
+    def close(self):
+        self.task.close()
+
 
 if __name__ == "__main__":
     q = 1
-    stim = Stim('Dev2/ao1', q)
+    #print(nidaqmx.system._collection
+    stim = Stim('Dev3/ao1', q)
     for i in range(5):
-        stim.sendStim
+        stim.sendStim()
+        time.sleep(1)
+    stim.close()
