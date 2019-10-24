ae6f5365328e41df45b41d97cdbe60b3dd5e11ac
Mark Schatza <markschatza@gmail.com>
2019-10-24 09:47:32 -0500

    TTL updates (working!)

--- BehGUI/zmqClasses.py
+++ BehGUI/zmqClasses.py
@@ -104,11 +104,14 @@ class SNDEvent:
         message = self.socket.recv()
         print("Received reply %s " %  message)
 
-    def sendTTL(ON_OFF, TTL_CHAN):
-        if ON_OFF:
-            self.socket.send(b"".join([b'TTL Channel=', TTL_CHAN, b' on=1']))
-        else:
-            self.socket.send(b"".join([b'TTL Channel=', TTL_CHAN, b' on=0']))
+    def sendTTL(self, TTL_CHAN):
+        # Send TTL ON
+        self.socket.send(b"".join([b'TTL Channel=', TTL_CHAN, b' on=1']))
+        self.socket.recv()
+        # Wait and send TTL OFF
+        time.sleep(0.005)
+        self.socket.send(b"".join([b'TTL Channel=', TTL_CHAN, b' on=0']))
+        self.socket.recv()
 
     # Function that acts as a C switch. Gets your desired string
     def switch(self, x):
