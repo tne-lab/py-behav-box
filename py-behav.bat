9949e48f5ea5a3348f5eb461ea92b579ab495eb9
zsong30 <you@example.com>
2021-11-10 11:28:28 -0600

    update activate.bat location

--- py-behav.bat
+++ py-behav.bat
@@ -1,4 +1,4 @@
-call C:\ProgramData\Anaconda3\Scripts\activate.bat py-behav-box
-cd C:\Users\ABET User\Documents\Github\py-behav-box\BehGUI
+call C:\Users\Neuropixel_01\anaconda3\Scripts\activate.bat py-behav-box
+cd C:\Users\Neuropixel_01\Documents\Github\py-behav-box\BehGUI
 call python BEH_GUI_MAIN.py
 pause
